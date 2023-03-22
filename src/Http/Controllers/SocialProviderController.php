35c29194f16a3b58c0ead3911cde744fdb2418d1
Ron Appleton <ron.appleton@arbor-education.com>
2023-03-22 23:49:58 +0000

    refactor: updates and refinements

--- src/Http/Controllers/SocialProviderController.php
+++ src/Http/Controllers/SocialProviderController.php
@@ -8,12 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Routing\Controller;
 use Larapkg\LaravelSocialUser\Http\Requests\SocialLoginRequest;
 use Larapkg\LaravelSocialUser\Models\UserSocialProvider;
-use Laravel\Socialite\Contracts\User as SocialUser;
 use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\One\User as OauthOneUser;
-use Laravel\Socialite\Two\User as OauthTwoUser;
-use Spatie\RouteAttributes\Attributes\Post;
-use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Laravel\Socialite\AbstractUser;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Throwable;
@@ -51,11 +47,7 @@ class SocialProviderController extends Controller
             'email' => $socialProviderUser->getEmail(),
         ]);
 
-        $socialProviderUser = $this->createUserProviderRecord($user, $socialProviderUser, $provider);
-
-        if (!$socialProviderUser) {
-            throw new AccessDeniedException(self::INVALID_PROVIDER);
-        }
+        $this->handleUserProviderRecord($user, $socialProviderUser, $provider);
 
         $token = $user->createToken(config('app.name'))->accessToken;
 
@@ -64,69 +56,22 @@ class SocialProviderController extends Controller
         ]);
     }
     
-    protected function createUserProviderRecord(
+    protected function handleUserProviderRecord(
         Model $user,
-        SocialUser $socialProviderUser,
+        AbstractUser $socialProviderUser,
         string $provider,
-    ): ?UserSocialProvider {
-        if ($socialProviderUser instanceof OauthOneUser) {
-            return $this->createOauthOneProviderRecord($user, $socialProviderUser, $provider);
-        }
-        
-        if (!($socialProviderUser instanceof OauthTwoUser)) {
-            return null;
-        }
-        
-        return $this->createOauthTwoProviderRecord($user, $socialProviderUser, $provider);
-    }
-    
-    protected function createOauthOneProviderRecord(
-        Model $user,
-        OauthOneUser $socialProviderUser,
-        string $provider,
-    ): UserSocialProvider {
+    ): void {
         $attributes = [
             'user_id' => $user->getAttribute('id'),
-            'provider_id' => $socialProviderUser->getId(),
+            'provider' => $provider,
         ];
 
         $values = [
-            'provider' => $provider,
+            'provider_id' => $socialProviderUser->getId(),
             'email' => $socialProviderUser->getEmail(),
             'avatar' => $socialProviderUser->getAvatar(),
         ];
 
-        return $this->createRecord($attributes, $values);
-    }
-    
-    protected function createOauthTwoProviderRecord(
-        Model $user,
-        OauthTwoUser $socialProviderUser,
-        string $provider,
-    ): UserSocialProvider {
-        $attributes = [
-            'user_id' => $user->getAttribute('id'),
-            'provider_id' => $socialProviderUser->getId(),
-        ];
-
-        $values = [
-            'provider' => $provider,
-            'email' => $socialProviderUser->getEmail(),
-            'avatar' => $socialProviderUser->getAvatar(),
-        ];
-        
-        return $this->createRecord($attributes, $values);
-    }
-
-    /**
-     * @param array<string, mixed> $attributes
-     * @param array<string, mixed> $values
-     */
-    protected function createRecord(array $attributes, array $values): UserSocialProvider
-    {
-        /** @var UserSocialProvider $userSocialProvider */
-        $userSocialProvider = UserSocialProvider::query()->updateOrCreate($attributes, $values);
-        
-        return $userSocialProvider;
+        UserSocialProvider::query()->updateOrCreate($attributes, $values);
     }
 }
